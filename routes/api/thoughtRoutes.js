5ebfd35630c5962a2779ccd30743e9a100869b36
xMaedayx <120540407+xMaedayx@users.noreply.github.com>
2023-05-02 15:06:08 -0700

    stash

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -6,6 +6,8 @@ const {
   deleteThought,
   addThought,
   removeThought,
+  addReactions,
+  
 
 } = require('../../controllers/thoughtsController');
 
@@ -19,5 +21,6 @@ router.route('/:thoughtId').get(getSingleThought).delete(deleteThought);
 router.route('/:thoughtId/post').post(addThought);
 
 router.route('/:thoughtId/post/:thoughtId').delete(removeThought);
+router.route('/:thoughtId/reactions').post(addReactions);
 
 module.exports = router;
